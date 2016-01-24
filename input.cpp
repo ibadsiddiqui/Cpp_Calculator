d10634545b2e7310ae11a4be053575132c7be616
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2016-01-24 07:59:28 +0500

    Update input.cpp

--- input.cpp
+++ input.cpp
@@ -10,22 +10,28 @@ main()
 	switch(no)
 	{
 		case 1:
+			system("cls");
 			a.input();
 			a.Basic();
 			break;
 		case 2:
+			system("cls");
 			a.squaring();
 			break;
 		case 3:
+			system("cls");
 			a.trignometry();
 			break;
 		case 4:
+			system("cls");
 			a.Base_N();
 			break;
 		case 5:
+			system("cls");
 			a.Matrix();
 			break;
 		case 6:
+			system("cls");
 			a.Equations();
 			break;
 	}
